4208e810268396a6588a291d14fbc8e8c6c378a5
tuan304201 <106050800+tuan304201@users.noreply.github.com>
2023-02-01 16:18:16 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 // See https://github.com/typicode/json-server#module
 const jsonServer = require('json-server')
-const queryString = require('node:querystring')
 const server = jsonServer.create()
 const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
@@ -11,43 +10,6 @@ server.use(jsonServer.rewriter({
     '/api/*': '/$1',
     '/blog/:resource/:id/show': '/:resource/:id'
 }))
-
-server.get('/echo', (req, res) => {
-    res.jsonp(req.query);
-});
-
-server.use(jsonServer.bodyParser);
-server.use((req, res, next) => {
-    if (req.method === 'POST') {
-        req.body.createdAt = Date.now();
-        req.body.updatedAt = Date.now();
-    } else if (req.method === 'PUT') {
-        req.body.updatedAt = Date.now();
-    }
-    // Continue to JSON Server router
-    next();
-});
-
-router.render = (req, res) => {
-    const headers = res.getHeaders();
-    const totalCountHeader = headers['x-total-count'];
-    if (req.method === 'GET' && totalCountHeader) {
-        const queryParams = queryString.parse(req._parsedUrl.query);
-        console.log(queryParams);
-
-        const result = {
-            data: res.locals.data,
-            pagination: {
-                _page: Number.parseInt(queryParams._page) || 1,
-                _limit: Number.parseInt(queryParams._limit) || 10,
-                _totalRows: Number.parseInt(totalCountHeader),
-            },
-        };
-
-        return res.jsonp(result);
-    }
-    res.jsonp(res.locals.data);
-};
 
 server.use('/api', router)
 server.listen(3000, () => {
